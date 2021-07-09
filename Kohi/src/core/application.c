50ae9e6ca4d8f8add1671dd9ecf193c36aae5af7
Nickyhub <nikolas.alexander.kaminsky@t-online.de>
2021-07-09 17:23:04 +0200

    Read access violation

--- Kohi/src/core/application.c
+++ Kohi/src/core/application.c
@@ -1,9 +1,10 @@
-#include "application.h"
+#include "core/application.h"
 #include "platform/platform.h"
 #include "logger.h"
 #include "gametypes.h"
-#include "kmemory.h"
+#include "core/kmemory.h"
 #include "core/event.h"
+#include "core/input.h"
 
 typedef struct application_state {
 	game* game_inst;
@@ -17,6 +18,10 @@ typedef struct application_state {
 static b8 initialized = FALSE;
 static application_state app_state;
 
+b8 application_on_event(u16 code, void* sender, void* listener_inst, event_context context);
+b8 application_on_key(u16 code, void* sender, void* listener_inst, event_context context);
+b8 application_on_key2(u16 code, void* sender, void* listener_inst, event_context context);
+
 b8 application_create(game* game_inst) {
 	if (initialized) {
 		EN_ERROR("application_create called more than once");
@@ -26,22 +31,19 @@ b8 application_create(game* game_inst) {
 	app_state.game_inst = game_inst;
 
 	initialize_logging();
-
-	EN_FATAL("Message: %f", 3.13131f);
-	EN_ERROR("Message: %f", 3.13131f);
-	EN_WARN("Message: %f", 3.13131f);
-	EN_DEBUG("Message: %f", 3.13131f);
-	EN_INFO("Message: %f", 3.13131f);
-	EN_TRACE("Message: %f", 3.13131f);
+	input_initialize();
 
 	app_state.is_running = TRUE;
 	app_state.is_suspended = FALSE;
 
 	if (!event_initialize()) {
-		EN_ERROR("Event system was not initialized!"); 
+		EN_ERROR("Event system failed initialization. Application cannot continue.");
 		return FALSE;
 	}
 
+	event_register(EVENT_CODE_APPLICATION_QUIT, 0, application_on_event);
+	event_register(EVENT_CODE_KEY_PRESSED, 0, application_on_key);
+	event_register(EVENT_CODE_KEY_RELEASED, 0, application_on_key);
 
 	if (!platform_startup(&app_state.platform,
 		game_inst->app_config.name,
@@ -84,13 +86,63 @@ b8 application_run() {
 				app_state.is_running = FALSE;
 				break;
 			}
-
-
+			//NOTE Inpute update/state copying should always be handled
+			//after any input should be recorded.; I.E. before this line.
+			//As a safety, input is the last thing to be updated beroe
+			//this frame ends.
+			input_update(0);
 		}
 	}
 
 	app_state.is_running = FALSE;
+
+	event_unregister(EVENT_CODE_APPLICATION_QUIT, 0, application_on_event);
+	event_unregister(EVENT_CODE_KEY_PRESSED, 0, application_on_key);
+	event_unregister(EVENT_CODE_KEY_RELEASED, 0, application_on_key);
+
 	event_shutdown();
+	input_shutdown();
 	platform_shutdown(&app_state.platform);
 	return TRUE;
+}
+
+b8 application_on_event(u16 code, void* sender, void* listener_inst, event_context context) {
+	switch (code) {
+	case EVENT_CODE_APPLICATION_QUIT:
+		EN_INFO("EVENT_CODE_APPLICATION_QUIT received, shutting down.\n");
+		app_state.is_running = FALSE;
+		return TRUE;
+	}
+}
+
+b8 application_on_key(u16 code, void* sender, void* listener_inst, event_context context) {
+	if (code == EVENT_CODE_KEY_PRESSED) {
+		u16 key_code = context.data.u16[0];
+		if (key_code == KEY_ESCAPE) {
+			event_context data = { 0 };
+			event_fire(EVENT_CODE_APPLICATION_QUIT, 0, data);
+			return TRUE;
+		}
+		else if (key_code == KEY_A) {
+			EN_DEBUG("Explicit - A key pressed!");
+		}
+		else {
+			EN_DEBUG("'%c' pressed in window.", key_code);
+		}
+	}
+	else if (code == EVENT_CODE_KEY_RELEASED) {
+		u16 key_code = context.data.u16[0];
+		if (key_code == KEY_B) {
+			EN_DEBUG("Explicit - B key released!");
+		}
+		else {
+			EN_DEBUG("'%c' key released in window.", key_code);
+		}
+	}
+	return FALSE;
+}
+
+b8 application_on_key2(u16 code, void* sender, void* listener_inst, event_context context) {
+	EN_FATAL("komische Registrierung funktioniert, guck halt mal im Debugger du Spast!!!!!!!");
+	return FALSE;
 }
--- Kohi/src/platform/platform_win32.c
+++ Kohi/src/platform/platform_win32.c
@@ -1,6 +1,7 @@
 #include "platform.h"
 #include "core/logger.h"
 #include <string.h>
+#include "core/input.h"
 
 #if KPLATFORM_WINDOWS
 #include <windows.h>
@@ -178,18 +179,24 @@ LRESULT CALLBACK win32_process_message(HWND hwnd, u32 message, WPARAM w_param, L
 	case WM_SYSKEYDOWN:
 	case WM_KEYUP:
 	case WM_SYSKEYUP: {
+		b8 pressed = (message == WM_KEYDOWN || message == WM_SYSKEYDOWN);
+		keys key = (u16)w_param;
+		input_process_key(key, pressed);
 
 	} break;
 
 	case WM_MOUSEMOVE: {
 		i32 x_position = GET_X_LPARAM(l_param);
 		i32 y_position = GET_Y_LPARAM(l_param);
+
+		input_process_mouse_move(x_position, y_position);
 	}
 	case WM_MOUSEWHEEL: {
 		i32 z_delta = GET_WHEEL_DELTA_WPARAM(w_param);
 		if (z_delta != 0) {
 			z_delta = (z_delta < 0) ? -1 : 1;
 		}
+		input_process_mouse_wheel((i8)z_delta);
 	} break;
 	case WM_LBUTTONDOWN:
 	case WM_MBUTTONDOWN:
@@ -198,6 +205,25 @@ LRESULT CALLBACK win32_process_message(HWND hwnd, u32 message, WPARAM w_param, L
 	case WM_MBUTTONUP:
 	case WM_RBUTTONUP: {
 		b8 pressed = message == WM_LBUTTONDOWN || message == WM_RBUTTONDOWN || message == WM_MBUTTONDOWN;
+		buttons mouse_button = BUTTON_MAX_BUTTONS;
+		switch (message)
+		{
+		case WM_LBUTTONDOWN:
+		case WM_LBUTTONUP:
+			mouse_button = BUTTON_LEFT;
+			break;
+		case WM_MBUTTONDOWN:
+		case WM_MBUTTONUP:
+			mouse_button = BUTTON_MIDDLE;
+			break;
+		case WM_RBUTTONDOWN:
+		case WM_RBUTTONUP:
+			mouse_button = BUTTON_RIGHT;
+			break;
+		}
+		if (mouse_button != BUTTON_MAX_BUTTONS) {
+			input_process_button(mouse_button, pressed);
+		}
 	} break;
 
 	}
